14fc30b93e3d951f517d1a09918fcfc0f720a725
soso-light <80017031+soso-light@users.noreply.github.com>
2021-12-18 02:01:03 +0900

    boardVO

--- HandongFood/src/main/java/com/handong/somoon/board/BoardVO.java
+++ HandongFood/src/main/java/com/handong/somoon/board/BoardVO.java
@@ -1,49 +1,103 @@
 package com.handong.somoon.board;
 
-import java.util.Date;
-
 public class BoardVO {
-	private int seq;
-	private String title;
-	private String writer;
-	private String content;
-	private Date regdate;
-	private int cnt;
+	/*
+	 * sid int AUTO_INCREMENT, 
+	 * userid varchar(20), 
+	 * username varchar(40),
+	 * password varchar(40), 
+	 * email varchar(60), 
+	 * blogurl varchar(100), 
+	 * photo varchar(30),
+	 * detail varchar(100), 
+	 * regdate timestamp default CURRENT_TIMESTAMP, 
+	 * primary key(sid)
+	 * 
+	 */
+	private int sid;
+	private String userid;
+	private String username;
+	private int star;
+	private int average_star;
+	private String address;
+	private String category;
+	private String url;
+	private String menu;
+	private String review;
+	private String photo;
+	private String regdate;
 	
-	public int getSeq() {
-		return seq;
+	public int getSid() {
+		return sid;
 	}
-	public void setSeq(int seq) {
-		this.seq = seq;
+	public void setSid(int sid) {
+		this.sid = sid;
 	}
-	public String getTitle() {
-		return title;
+	public String getUserid() {
+		return userid;
 	}
-	public void setTitle(String title) {
-		this.title = title;
+	public void setUserid(String userid) {
+		this.userid = userid;
 	}
-	public String getWriter() {
-		return writer;
+	public String getUsername() {
+		return username;
 	}
-	public void setWriter(String writer) {
-		this.writer = writer;
+	public void setUsername(String username) {
+		this.username = username;
 	}
-	public String getContent() {
-		return content;
+	public int getStar() {
+		return star;
 	}
-	public void setContent(String content) {
-		this.content = content;
+	public void setStar(int star) {
+		this.star = star;
 	}
-	public Date getRegdate() {
+	public int getAverage_star() {
+		return average_star;
+	}
+	public void setAverage_star(int average_star) {
+		this.average_star = average_star;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getCategory() {
+		return category;
+	}
+	public void setCategory(String category) {
+		this.category = category;
+	}
+	public String getUrl() {
+		return url;
+	}
+	public void setUrl(String url) {
+		this.url = url;
+	}
+	public String getMenu() {
+		return menu;
+	}
+	public void setMenu(String menu) {
+		this.menu = menu;
+	}
+	public String getReview() {
+		return review;
+	}
+	public void setReview(String review) {
+		this.review = review;
+	}
+	public String getPhoto() {
+		return photo;
+	}
+	public void setPhoto(String photo) {
+		this.photo = photo;
+	}
+	public String getRegdate() {
 		return regdate;
 	}
-	public void setRegdate(Date regdate) {
+	public void setRegdate(String regdate) {
 		this.regdate = regdate;
 	}
-	public int getCnt() {
-		return cnt;
-	}
-	public void setCnt(int cnt) {
-		this.cnt = cnt;
-	}
+
 }
